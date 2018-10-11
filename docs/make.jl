24ce99cb78028b33495cc0e1ab3c9fa381462fb2
Ciarán O’Mara <CiaranOMara@utas.edu.au>
2018-10-11 12:08:36 +1100

    Remove IO from documentation

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,6 @@ makedocs(
     pages = [
         "Home" => "index.md",
         "Intervals" => "intervals.md",
-        "I/O" => [
-            "BED" => "io/bed.md",
-            "GFF3" => "io/gff3.md",
-            "BigWig" => "io/bigwig.md",
-            "BigBed" => "io/bigbed.md"
-        ]
     ],
     authors = "Kenta Sato, D. C. Jones, Ben J. Ward, The BioJulia Organisation and other contributors."
 )
