ffbe7dc28071b9690177b9bd8342e2c3c1e53e78
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-08-12 12:29:38 +1000

    Update make docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,19 +1,23 @@
+using Pkg
+
+Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
+
 using Documenter, GenomicFeatures
 
 makedocs(
-    format = :html,
+    format = Documenter.HTML(
+        edit_branch = "develop"
+    ),
+    modules = [GenomicFeatures],
     sitename = "GenomicFeatures.jl",
     pages = [
         "Home" => "index.md",
         "Intervals" => "intervals.md",
     ],
-    authors = "Kenta Sato, D. C. Jones, Ben J. Ward, The BioJulia Organisation and other contributors."
+    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors."
 )
 deploydocs(
     repo = "github.com/BioJulia/GenomicFeatures.jl.git",
-    julia = "0.6",
-    osname = "linux",
-    target = "build",
-    deps = nothing,
-    make = nothing
+    devbranch = "develop"
 )
