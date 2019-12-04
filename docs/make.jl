ee66a2d949ef8da0ec0f190f3b83994c5d7f1d92
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2019-12-04 21:46:59 +1100

    Update Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,15 @@
+using Pkg
+
+Pkg.develop(PackageSpec(path=pwd()))
+Pkg.instantiate()
+
 using Documenter, GenomicFeatures
 
 makedocs(
-    format = :html,
+    format = Documenter.HTML(
+        edit_link = :commit
+    ),
+    modules = [GenomicFeatures],
     sitename = "GenomicFeatures.jl",
     pages = [
         "Home" => "index.md",
@@ -13,13 +21,9 @@ makedocs(
             "BigBed" => "io/bigbed.md"
         ]
     ],
-    authors = "Kenta Sato, D. C. Jones, Ben J. Ward, The BioJulia Organisation and other contributors."
+    authors = replace(join(Pkg.TOML.parsefile("Project.toml")["authors"], ", "), r" <.*?>" => "" ) * ", The BioJulia Organisation, and other contributors."
 )
 deploydocs(
     repo = "github.com/BioJulia/GenomicFeatures.jl.git",
-    julia = "0.6",
-    osname = "linux",
-    target = "build",
-    deps = nothing,
-    make = nothing
+    devbranch = "develop"
 )
