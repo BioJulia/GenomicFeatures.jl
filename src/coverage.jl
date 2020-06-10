997132f94e654077a38c29f9c97570010cb2bfcf
Ciarán O'Mara <Ciaran.OMara@utas.edu.au>
2020-06-10 15:40:54 +1000

    Coverage example

--- src/coverage.jl
+++ src/coverage.jl
@@ -19,6 +19,23 @@ For example, given intervals like:
 This function would return a new set of disjoint intervals with annotated coverage like:
 
     [1][-2-][-1-][--2--][--1--]
+
+# Example
+
+```jldoctest
+julia> intervals = [
+           Interval("chr1", 1, 8),
+           Interval("chr1", 4, 20),
+           Interval("chr1", 14, 27)];
+
+julia> coverage(intervals)
+IntervalCollection{UInt32} with 5 intervals:
+  chr1:1-3  .  1
+  chr1:4-8  .  2
+  chr1:9-13  .  1
+  chr1:14-20  .  2
+  chr1:21-27  .  1
+```
 """
 function coverage(stream, seqname_isless::Function=isless)
     cov = IntervalCollection{UInt32}()
